2c7af46e4385da4675599f70d23127d5fb34499b
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-02-28 01:29:50 +0200

    Mongoose init , set db name and connection

---
+++ server/src/config/mongooseInit.js
@@ -0,0 +1,13 @@
+import { connect } from "mongoose";
+
+export default async function mongooseInit() {
+    try {
+        const url = process.env.DB_URL;
+        await connect(url, {dbName: 'FeastTogether'});
+        console.log('Successfully connect to DB!');
+        
+    } catch (err) {
+        console.log('Cannot connect to DB!' + err.message);
+        
+    }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import expressInit from './config/expressInit.js'
 import { corsMiddleware } from "./middlewares/cors.js";
+import 'dotenv/config'
+import mongooseInit from "./config/mongooseInit.js"
 
 const app = express();
 
+
+
 app.use(corsMiddleware);
+mongooseInit();
+expressInit(app);
 
 
-expressInit(app)
+app.listen(process.env.PORT , () => console.log(`Server is running on port: ${process.env.PORT}`));
