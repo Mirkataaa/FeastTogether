850d15bd236026d153e1f70ea44bad9e69fa43c9
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-02-28 04:49:27 +0200

    Fixed express init

--- server/src/config/expressInit.js
+++ server/src/config/expressInit.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import routes from "../routes.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 export default function expressInit(app) {
-    
-    app.use(cookieParser);
-    app.use(express.json);
+    app.use(cookieParser());
+    app.use(express.json());
+    app.use(authMiddleware);
     app.use(routes);
+}
 
-};
 
