33105ed02a74cd6e7929987ef0a58eb3932dcccb
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-02-28 18:10:56 +0200

    Add wildcard

--- server/src/routes.js
+++ server/src/routes.js
@@ -8,4 +8,10 @@ const routes = Router();
 routes.use('/api/', homeController);
 routes.use('/api/user', userController);
 
+
+routes.all('*' , (req,res) => {
+    res.status(404).json({message: '404 - Page not found'});
+});
+
+
 export default routes;
