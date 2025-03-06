a00d45dc9edf8ffb2275e6c83f7b0a9082271d59
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-03-07 01:40:52 +0200

    Updated routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,13 +2,17 @@ import { Router } from "express";
 import userController from "./controllers/userController.js";
 import homeController from "./controllers/homeController.js";
 import recipeController from "./controllers/recipeController.js";
+import commentController from "./controllers/commentController.js";
+import ratingController from "./controllers/ratingController.js";
 
 
 const routes = Router();
 
 routes.use('/api/', homeController);
 routes.use('/api/user', userController);
-routes.use('/api/recipes' , recipeController)
+routes.use('/api/recipes' , recipeController);
+routes.use('/api/comments', commentController);
+routes.use('/api/ratings' , ratingController);
 
 
 routes.all('*' , (req,res) => {
