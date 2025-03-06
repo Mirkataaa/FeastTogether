2261a40df019b9a69fe198966cf341464470033d
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-03-06 17:21:37 +0200

    Add recipe controller to routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import userController from "./controllers/userController.js";
 import homeController from "./controllers/homeController.js";
+import recipeController from "./controllers/recipeController.js";
 
 
 const routes = Router();
 
 routes.use('/api/', homeController);
 routes.use('/api/user', userController);
+routes.use('/api/recipes' , recipeController)
 
 
 routes.all('*' , (req,res) => {
