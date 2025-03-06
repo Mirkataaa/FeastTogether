b356146d8c831cf9f1ebe6adadaa79c0bdcb5bd2
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-03-07 00:17:27 +0200

    Add comments service

---
+++ server/src/services/commentService.js
@@ -0,0 +1,72 @@
+import Comment from "../models/Comment.js";
+import Recipe from "../models/Recipes.js";
+
+const getCommentsByUser  = async (userId) => {
+    try {
+        const comments = await Comment.find({user: userId}).populate('recipe' , 'title');
+        return comments;
+    } catch (error) {
+        throw new Error('Error adding comment' + error.message);       
+    }
+}
+
+const addComment = async (recipeId , commentData) => {
+    try {
+        const comment = new Comment(commentData);
+        await comment.save();
+
+        const recipe = await Recipe.findByIdAndUpdate(
+            recipeId,
+            {$push: {comment: comment._id}},
+            {new: true}
+        ).populate('comments');
+
+        return recipe;
+    } catch (error) {
+        throw new Error('Error adding comment' + error.message);
+    }
+};
+
+const updateComment = async (commentId , newText) => {
+    try {
+        const comment = await Comment.findById(commentId);
+        
+        if(!comment) {
+            throw new Error('Comment not found!');
+        }
+
+        comment.text = newText;
+        await comment.save();
+        return comment;
+    } catch (error) {
+        throw new Error('Error adding comment' + error.message);
+    }
+}
+
+const deleteComment = async (recipeId , commentId) => {
+    try {
+        const comment = await Comment.findById(commentId);
+        if(!comment) {
+            throw new Error('Comment not found!');
+        }
+
+        await comment.remove();
+
+        const recipe = await Recipe.findByIdAndUpdate(
+            recipeId,
+            {$pull: {comments: commentId}},
+            {new: true}
+        ).populate('comments')
+
+        return recipe;
+    } catch (error) {
+        throw new Error('Error adding comment' + error.message);
+    }
+}
+
+export default {
+    getCommentsByUser,
+    addComment,
+    updateComment,
+    deleteComment
+}
