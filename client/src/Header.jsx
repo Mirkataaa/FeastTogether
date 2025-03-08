ae1ac7080d6623e0a40d0bda012e5a584da71d46
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-03-08 14:15:55 +0200

    Install,config and test tailwind

---
+++ client/src/Header.jsx
@@ -0,0 +1,17 @@
+
+export function Header () {
+    return (
+        <>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+          Feast
+          <span className="font-bold">Together</span>
+        </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-orange-700 text-white rounded-full p-2 text-bold">
+            Lorem
+          </p>
+          <p className="p-2 text-bold">Lorem ipsum</p>
+        </div>
+        </>
+    ); 
+}
