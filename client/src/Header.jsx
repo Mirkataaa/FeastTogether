971c1b69cd8aa40f3eb12f4a7184332a2bdc52b0
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-03-09 19:05:03 +0200

    Add basic header component

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,17 +1,30 @@
+import {FaAlignJustify} from 'react-icons/fa'
+import {AiOutlineSearch} from 'react-icons/ai'
 
 export function Header () {
+  
     return (
-        <>
+      <div className="bg-tea-green">
+      <div className="max-w-[1720px] mx-auto flex justify-between items-center p-4">
+      <div className="flex items-center">
+        <div className="cursor-pointer hover:scale-130 transition-transform duration-200 ease-in-out">
+        <FaAlignJustify size={25} />
+        </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
           Feast
-          <span className="font-bold">Together</span>
+          <span className="font-bold text-glaucous">Together</span>
         </h1>
-        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
-          <p className="bg-orange-700 text-white rounded-full p-2 text-bold">
-            Lorem
-          </p>
-          <p className="p-2 text-bold">Lorem ipsum</p>
-        </div>
-        </>
+      </div>
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+         {/* // TODO: Search components ??? + functionality*/}
+        <AiOutlineSearch size={25} />
+        <input
+          className="bg-transparent p-2 w-full focus:outline-none"
+          type="text"
+          placeholder="search meals"
+        />
+      </div>
+      </div>
+      </div>
     ); 
 }
