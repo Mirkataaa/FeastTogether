9692edb20087e21d6425980ecdd2f5cd95adcf35
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-03-13 10:50:18 +0200

    Add react-router and signUp button to the hader

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,51 +2,55 @@ import { useState } from 'react';
 
 
 
-import {FaAlignJustify} from 'react-icons/fa'
-import {AiOutlineSearch} from 'react-icons/ai'
+import { FaAlignJustify } from 'react-icons/fa'
+import { AiOutlineSearch } from 'react-icons/ai'
 import SideNav from './side-nav/SideNav';
+import { Link } from 'react-router';
 
-export default function Header () {
+export default function Header() {
 
-    const [sideNav , setSideNav] = useState(false);
+  const [sideNav, setSideNav] = useState(false);
 
-    const sideNavToggleHandler = () => {
-      setSideNav((currentState) => !currentState)
-    }
+  const sideNavToggleHandler = () => {
+    setSideNav((currentState) => !currentState)
+  }
 
-    const renderOverlay = () => {
-      return (
-        <div
-          className="bg-black/60 fixed w-full h-screen z-10 top-0 left-0"
-          onClick={sideNavToggleHandler}
-        ></div>
-      );
-    };
-  
+  const renderOverlay = () => {
     return (
-      <div className="bg-tea-green">
+      <div
+        className="bg-black/60 fixed w-full h-screen z-10 top-0 left-0"
+        onClick={sideNavToggleHandler}
+      ></div>
+    );
+  };
+
+  return (
+    <div className="bg-tea-green">
       <div className="w-full mx-auto flex justify-between items-center p-4">
-      <div className="flex items-center">
-        <div onClick={sideNavToggleHandler} className="cursor-pointer hover:scale-130 transition-transform duration-200 ease-in-out">
-        <FaAlignJustify size={25} />
+        <div className="flex items-center">
+          <div onClick={sideNavToggleHandler} className="cursor-pointer hover:scale-130 transition-transform duration-200 ease-in-out">
+            <FaAlignJustify size={25} />
+          </div>
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
+            Feast
+            <span className="font-bold text-glaucous">Together</span>
+          </h1>
         </div>
-        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
-          Feast
-          <span className="font-bold text-glaucous">Together</span>
-        </h1>
+        <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+          {/* // TODO: Search components ??? + functionality*/}
+          <AiOutlineSearch size={25} />
+          <input
+            className="bg-transparent p-2 w-full focus:outline-none"
+            type="text"
+            placeholder="search meals"
+          />
+        </div>
+        <Link to="/register" className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded-full">
+          SignUp
+        </Link>
       </div>
-      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
-         {/* // TODO: Search components ??? + functionality*/}
-        <AiOutlineSearch size={25} />
-        <input
-          className="bg-transparent p-2 w-full focus:outline-none"
-          type="text"
-          placeholder="search meals"
-        />
-      </div>
-      </div>
-      {sideNav && <SideNav toggleSideNav={sideNavToggleHandler}/> }
+      {sideNav && <SideNav toggleSideNav={sideNavToggleHandler} />}
       {sideNav && renderOverlay()}
-      </div>
-    ); 
+    </div>
+  );
 }
