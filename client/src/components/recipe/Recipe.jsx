5d5b2c308d897c6eb36301a1894569e569872787
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-03-21 14:46:14 +0200

    Refactor recipe catalog to use recipeApi

--- client/src/components/recipe/Recipe.jsx
+++ client/src/components/recipe/Recipe.jsx
@@ -1,124 +1,18 @@
+import { useRecipes } from "../../api/recipeApi";
 import RecipeCard from "./recipe-card/RecipeCard";
-
-const recipes = [
-  {
-    title: "Chocolate Cake Pops",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Desserts", "Pastry"],
-  },
-  {
-    title: "Spicy Gnocchi Bake",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Comfort Food", "Quick Meal"],
-  },
-  {
-    title: "Fresh Avocado Toast",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Healthy", "Breakfast"],
-  },
-  {
-    title: "Chocolate Cake Pops",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Desserts", "Pastry"],
-  },
-  {
-    title: "Spicy Gnocchi Bake",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Comfort Food", "Quick Meal"],
-  },
-  {
-    title: "Fresh Avocado Toast",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Healthy", "Breakfast"],
-  },
-  {
-    title: "Chocolate Cake Pops",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Desserts", "Pastry"],
-  },
-  {
-    title: "Spicy Gnocchi Bake",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Comfort Food", "Quick Meal"],
-  },
-  {
-    title: "Fresh Avocado Toast",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Healthy", "Breakfast"],
-  },
-  {
-    title: "Chocolate Cake Pops",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Desserts", "Pastry"],
-  },
-  {
-    title: "Spicy Gnocchi Bake",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Comfort Food", "Quick Meal"],
-  },
-  {
-    title: "Fresh Avocado Toast",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Healthy", "Breakfast"],
-  },
-  {
-    title: "Chocolate Cake Pops",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Desserts", "Pastry"],
-  },
-  {
-    title: "Spicy Gnocchi Bake",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Comfort Food", "Quick Meal"],
-  },
-  {
-    title: "Fresh Avocado Toast",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Healthy", "Breakfast"],
-  },
-  {
-    title: "Chocolate Cake Pops",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Desserts", "Pastry"],
-  },
-  {
-    title: "Spicy Gnocchi Bake",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Comfort Food", "Quick Meal"],
-  },
-  {
-    title: "Fresh Avocado Toast",
-    image:
-      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574",
-    tags: ["Healthy", "Breakfast"],
-  },
-];
-
-// ? Add fav list ?
   
   export function Recipe() {
+    const {recipes} = useRecipes();
+
+    console.log(recipes);
+    
+
     return (
-      <div className="max-w-6xl mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 auto-rows-[180px] ">
-        {recipes.map((recipe, index) => (
-          <RecipeCard key={index} recipe={recipe} index={index} /> // ! change key with id later 
-        ))}
-      </div>
+      <section className="max-w-6xl mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 auto-rows-[180px] ">
+        {recipes.length > 0
+        ? recipes.map((recipe , index) => <RecipeCard key={recipe._id} {...recipe} index={index} />)
+        : <h3>No Recipes</h3>
+        }
+      </section>
     );
   }
--- client/src/components/recipe/recipe-card/RecipeCard.jsx
+++ client/src/components/recipe/recipe-card/RecipeCard.jsx
@@ -1,18 +1,18 @@
-
-
-export default function RecipeCard ({ recipe , index }) {
+export default function RecipeCard ({
+  title,
+  imageUrl,
+  index,
+  category
+}) {
     const cardSize = ["row-span-1" , "row-span-2" , "row-span-3"];
 
     return (
       <div className={`bg-white rounded-xl shadow-lg overflow-hidden flex flex-col ${cardSize[index % cardSize.length]} transform transition duration-300 hover:scale-105 group cursor-pointer`}>
-        <img src={recipe.image} alt={recipe.title} className="w-full h-full object-cover" />
+        <img src={imageUrl} alt={title} className="w-full h-full object-cover" />
         <div className="p-4">
-            {/* // ! add conditional underline (whole card) */}
-          <h3 className="text-lg font-semibold group-hover:underline decoration-green-500 transition-all">{recipe.title}</h3>
+          <h3 className="text-lg font-semibold group-hover:underline decoration-green-500 transition-all">{title}</h3>
           <div className="mt-2 flex flex-wrap gap-2">
-            {recipe.tags.map((tag, index) => (
-              <span key={index} className="bg-green-100 text-green-700 text-xs px-2 py-1 rounded-full">{tag}</span>
-            ))}
+              <span key={index} className="bg-green-100 text-green-700 text-xs px-2 py-1 rounded-full">{category}</span>
           </div>
         </div>
       </div>
