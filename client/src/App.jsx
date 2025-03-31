60a86faa2999610d43c025ddfa3ac70e99342bfe
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-04-01 02:59:27 +0300

    Add route guards , update app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,12 @@ import { Recipe } from './components/recipe/Recipe'
 import CreateRecipe from './components/create-recipe/CreateRecipe'
 import UserProvider from './providers/UserProvider'
 import RecipeDetails from './components/recipe-details/RecipeDetails'
+import Logout from './components/logout/Logout'
+import BrowseCategories from './components/browse-categoreis/BrowseCategories'
+import EditRecipe from './components/edit-recipe/EditRecipe'
+import HomeRecipe from './components/home/HomeRecipe'
+import AuthGuard from './components/guards/AuthGuard'
+import GuestGuard from './components/guards/GuestGuard'
 
 function App() {
   return (
@@ -17,11 +23,20 @@ function App() {
       <Header/>
       <main className='main-content'>
         <Routes>
+          <Route index element={<HomeRecipe/>}/>
           <Route path='/recipes/all-recipes' element={<Recipe/>}/>
           <Route path='/recipes/:recipeId' element= {<RecipeDetails/>} />
-          <Route path='/recipes/create' element={<CreateRecipe/>}/>
-          <Route path='/register' element={<Register/>} />
+          <Route path='/recipes/category/:category' element={<BrowseCategories/>} />
+          <Route element={<AuthGuard />}>
+              <Route path='/recipes/create' element={<CreateRecipe/>}/>
+              <Route path='/recipes/edit/:recipeId' element={<EditRecipe/>} />
+              <Route path='/logout' element={<Logout/>} />
+          </Route>
+          <Route element={<GuestGuard />}>
           <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+
+          </Route>
         </Routes>
       </main>
       <Footer/>
