6760e2dadbdb06fb78af538663a5ec4b28f74b2a
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-04-01 05:01:17 +0300

    Refactor browse categories

--- client/src/components/browse-categoreis/BrowseCategories.jsx
+++ client/src/components/browse-categoreis/BrowseCategories.jsx
@@ -6,24 +6,28 @@ export default function BrowseCategories() {
     const { data } = useRecipeCategory(category);
 
     return (
-        <section className="max-w-6xl mx-auto columns-2 md:columns-3 lg:columns-4 xl:columns-4 gap-4 m-5 ">
-            {data.recipes.length > 0 ? (
-                data.recipes.map(recipe => (
-                    <Link to={`/recipes/${recipe._id}`} key={recipe._id}>
-                        <div className="bg-white rounded-xl shadow-lg overflow-hidden flex flex-col transform transition duration-300 hover:scale-105 group cursor-pointer mb-5">
-                            <img src={recipe.imageUrl} alt={recipe.title} className="w-full h-full object-cover" />
-                            <div className="p-4">
-                                <h3 className="text-lg font-semibold group-hover:underline decoration-green-500 transition-all">{recipe.title}</h3>
-                                <div className="mt-2 flex flex-wrap gap-2">
-                                    <span className="bg-green-200 text-green-700 text-xs px-3 py-1 rounded-full w-fit mt-2">{category}</span>
-                                </div>
+        <section className="max-w-7xl mx-auto px-4 py-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {data.recipes.length > 0 ? (
+            data.recipes.map((recipe) => (
+                <Link to={`/recipes/${recipe._id}`} key={recipe._id}>
+                    <div className="bg-white rounded-xl shadow-lg overflow-hidden transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-xl group cursor-pointer">
+                        <img
+                            src={recipe.imageUrl}
+                            alt={recipe.title}
+                            className="w-full h-56 object-cover transition-transform duration-300 group-hover:scale-110"
+                        />
+                        <div className="p-4 space-y-4">
+                            <h3 className="text-lg font-semibold text-gray-900 group-hover:text-green-600 transition-colors duration-300">{recipe.title}</h3>
+                            <div className="flex flex-wrap gap-2">
+                                <span className="bg-green-200 text-green-700 text-xs px-3 py-1 rounded-full">{category}</span>
                             </div>
                         </div>
-                    </Link>
-                ))
-            ) : (
-                <p>No recipes found.</p>
-            )}
-        </section>
+                    </div>
+                </Link>
+            ))
+        ) : (
+            <p className="text-center text-xl text-gray-500">No recipes found.</p>
+        )}
+    </section>
     );
 };
